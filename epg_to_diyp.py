6882acb2511f730dca52809020d528678924ad67
fxq12345 <1414108336@qq.com>
2026-09-22 18:36:14 +0800

    Add files via upload

---
+++ epg_to_diyp.py
@@ -0,0 +1,7 @@
+你的仓库根目录
+├─ epg.py               # 你原来旧的EPG脚本
+├─ config.txt           # 旧脚本配置
+├─ epg_to_diyp.py       # 【新增】DIYP转换脚本
+└─ .github/
+   └─ workflows/
+      └─ diyp‑epg.yml   # 【新增】流水线配置
